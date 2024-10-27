582988d43b27b72dbf941fff76d0d3e62bdda927
rw4n99 <rowangodfrey2@gmail.com>
2024-10-27 11:14:35 +0000

    added alt

--- carpool/src/components/Header/Header.jsx
+++ carpool/src/components/Header/Header.jsx
@@ -7,12 +7,12 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <Link href={"/"}>
-                <Image src="/images/Nimbus.png" width={150} height={50} />
+                <Image src="/images/Nimbus.png" alt='hi' width={150} height={50} />
             </Link>
             <div className={styles.profile}>
                 <p>Points: 9001</p>
             <Link href={"/profile"}>
-                <Image src="/images/profile.svg" width={30} height={30} />
+                <Image src="/images/profile.svg" alt='hi' width={30} height={30} />
             </Link>
             </div>
         </header>
