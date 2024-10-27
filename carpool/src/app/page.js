392367784e22278109879db03555f5ec75553158
James O'Kane <orbiccode@gmail.com>
2024-10-27 07:12:29 +0000

    Test Commit

--- carpool/src/app/page.js
+++ carpool/src/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
-import Footer from "../components/Footer/Footer";
+import BookingForm from "../components/BookingForm/BookingForm"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Carsharing</h1>
+      <h1 className={styles.h1}>Add a Trip</h1>
+        <BookingForm />
+        <div className={styles.green}></div>
     </div>
   );
 }
---
+++ carpool/src/components/BookingForm/BookingForm.jsx
@@ -0,0 +1,12 @@
+import styles from "./BookingForm.module.css"
+
+export default function BookingForm() {
+    return (
+            <form className={styles.form}>
+                <input className={styles.input} type="text" placeholder="Leaving from"></input>
+                <input className={styles.input} type="text" placeholder="Going to"></input>
+                <input className={styles.input} type="date" placeholder="When?"></input>
+                <input className={styles.input} type="text"></input>
+            </form>
+    )
+}
