55c15942085bde6fe7b71ee263faa7a486c9c2d6
James O'Kane <orbiccode@gmail.com>
2024-10-27 09:30:07 +0000

    Test 1

--- carpool/src/app/page.js
+++ carpool/src/app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import BookingForm from "../components/BookingForm/BookingForm"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.h1}>Add a Trip</h1>
-        <BookingForm />
-        <div className={styles.green}></div>
+    <div className={styles.imageWrapper}>
+      <div className={styles.image}></div>
+      <Link href={"/start"}>
+          <button className={styles.button}>Add Trip</button>
+      </Link>
     </div>
   );
 }
--- carpool/src/app/start/page.js
+++ carpool/src/app/start/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import BookingForm from "@/components/BookingForm/BookingForm";
+import styles from "./page.module.css"
 
 
-export default function Home() {
+export default function Start() {
   return (
-    <div>
-      <h1>Start Your commute</h1>
-      
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Add a Trip</h1>
+        <BookingForm />
+        <div className={styles.green}></div>
     </div>
   );
 }
